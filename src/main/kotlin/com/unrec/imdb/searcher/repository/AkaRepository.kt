fcf376422188623ab5468e806a34a589016b133c
unrec <andrei.troitskiy@gmail.com>
2022-05-24 13:28:38 +0300

    jpa related classes deleted

--- src/main/kotlin/com/unrec/imdb/searcher/repository/AkaRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.unrec.imdb.searcher.repository
-
-import com.unrec.imdb.searcher.model.Aka
-import com.unrec.imdb.searcher.model.AkaId
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AkaRepository : JpaRepository<Aka, AkaId>
--- src/main/kotlin/com/unrec/imdb/searcher/repository/EpisodeRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.unrec.imdb.searcher.repository
-
-import com.unrec.imdb.searcher.model.Episode
-import com.unrec.imdb.searcher.model.EpisodeId
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface EpisodeRepository : JpaRepository<Episode, EpisodeId>
